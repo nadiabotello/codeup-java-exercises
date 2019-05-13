8223e0c6f67ff61ab454ab511b68d2e1f693efa2
Nadia Botello <botello.nadia.g@gmail.com>
2019-05-13 11:19:13 -0500

    completed bonus

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.print("Hello");
-        System.out.print(", World");
+        System.out.println("Hello, World");
     }
 }
