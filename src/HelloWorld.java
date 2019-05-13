06bc9a7ad08e89062f114d8d0022f32f9d551dfa
Nadia Botello <botello.nadia.g@gmail.com>
2019-05-13 12:28:11 -0500

    syntax exercise lunch push

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,14 @@
 public class HelloWorld {
+
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+//        System.out.println("Hello, World");
+
+        int myFavoriteNumber = 9;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "Cats love strings";
+        System.out.println(myString);
+
+
     }
 }
