60f91b8bdc09c9e131ac86905e7871d5a9040f51
Nadia Botello <botello.nadia.g@gmail.com>
2019-05-14 14:06:51 -0500

    do while 2 increment

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,11 +2,12 @@ public class ControlFlowExercises {
     public static void main(String[] args) {
 
 
-        int i = 5;
-        while (i <= 15) {
-            System.out.println("i is " + i);
-            i++;
-        }
+        int i = 0;
+
+        do {
+            System.out.println("i = " + i);
+            i += 2;
+        } while (i <= 100);
 
 
     }
