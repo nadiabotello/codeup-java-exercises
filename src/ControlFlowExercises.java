b4eb6347623553ec78cdee461a1df238e0336df8
Nadia Botello <botello.nadia.g@gmail.com>
2019-05-14 14:16:11 -0500

    for for five

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,16 +2,9 @@ public class ControlFlowExercises {
     public static void main(String[] args) {
 
 
-        long i = 2;
-
-        do {
-            System.out.println("i = " + i);
-            i *= i;
-        } while (i < 1000000);
-
-//        for(long i = 2; i < 1000000; i *= i) {
-//            System.out.println(i);
-//        }
+        for(int i = 5; i <= 15; i++) {
+            System.out.println(i);
+        }
 
 
 
