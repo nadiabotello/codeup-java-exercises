ba5077faada9ccb9ed4051abb671c5e13e5de9bd
Nadia Botello <botello.nadia.g@gmail.com>
2019-05-14 14:23:51 -0500

    fizzbuzz

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,8 +2,20 @@ public class ControlFlowExercises {
     public static void main(String[] args) {
 
 
-        for(int i = 5; i <= 15; i++) {
-            System.out.println(i);
+        for(int i = 0; i <= 100; i++) {
+                if(i % 3 == 0) {
+                    System.out.println("Fizz");
+                    continue;
+                } else if (i % 5 == 0) {
+                    System.out.println("Buzz");
+                    continue;
+                } else if (i % 3 == 0 & i % 5 == 0) {
+                    System.out.println("FizzBuzz");
+                    break;
+                }
+
+                System.out.println(i);
+
         }
 
 
