9a93a3a6e11524763b715c4af80bf9ed65926e74
Nadia Botello <botello.nadia.g@gmail.com>
2019-05-14 14:27:39 -0500

    fixed 15

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -3,19 +3,19 @@ public class ControlFlowExercises {
 
 
         for(int i = 0; i <= 100; i++) {
+
                 if(i % 3 == 0) {
                     System.out.println("Fizz");
                     continue;
                 } else if (i % 5 == 0) {
                     System.out.println("Buzz");
                     continue;
-                } else if (i % 3 == 0 & i % 5 == 0) {
+                } else if (i % 15 == 0) {
                     System.out.println("FizzBuzz");
                     break;
                 }
 
-                System.out.println(i);
-
+            System.out.println(i);
         }
 
 
