02c5bdbf377770d1e691df46802d70f0e198808d
Nadia Botello <botello.nadia.g@gmail.com>
2019-05-16 11:27:56 -0500

    finished methods

--- src/HighLow.java
+++ src/HighLow.java
@@ -3,36 +3,37 @@ import java.util.Scanner;
 
 public class HighLow {
     public static void main(String[] args) {
-        System.out.print("Guess a number. \n");
+        System.out.println("Guess a number. \n");
         rollValidation();
-
     }
+
 
     public static void rollValidation() {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
 
-        int num = scanner.nextInt();
+        int num = Integer.parseInt(scanner.nextLine());
         int randomNumber = random.nextInt(100);
 
+        do {
 
             if (num >= 1 & num <= 100) {
-//                System.out.println("You are within range.");
+                if (num >= randomNumber) {
+                    System.out.println("LOWER");
+                    System.out.println(randomNumber + "\n");
+                } else if (num <= randomNumber) {
+                    System.out.println("HIGHER");
+                    System.out.println(randomNumber + "\n");
+                } else {
+                    break;
+                }
 
-//                do {
-                    if (num >= randomNumber) {
-                        System.out.println("LOWER");
-                    } else {
-                        System.out.println("HIGHER");
-                    }
-//
-//                } while (true);
-
-
-            } else {
-                System.out.print("Please enter a valid number between 1 and 100.");
+                System.out.print("Make another guess. \n");
+                num = Integer.parseInt(scanner.nextLine());
             }
 
+        } while (num != randomNumber);
+        System.out.println("Good guess! You win. \n");
     }
 
 
