47e13907671d240fe85a7ea62bef28b31ab2480b
Nadia Botello <botello.nadia.g@gmail.com>
2019-05-17 11:42:51 -0500

    updated

--- src/Person.java
+++ src/Person.java
@@ -8,17 +8,21 @@ public class Person {
 
     public void setName(String name) {
         this.name = name;
+        System.out.println("Your new name is: " + name);
     }
 
     public void sayHello() {
-        System.out.printf("Hi from: " + name);
+        System.out.printf("Hi from: " + name + "\n");
     }
 
     public static void main(String[] args) {
 
         Person nadia = new Person();
         nadia.name = "Nadia";
+
         nadia.sayHello();
+        System.out.println(nadia.getName());
+        nadia.setName("Fer");
     }
 
 
