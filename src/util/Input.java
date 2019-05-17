ee5347dcb99879135f6ecc2384c5cd03d9bc7b16
Nadia Botello <botello.nadia.g@gmail.com>
2019-05-17 14:50:32 -0500

    exercise 3

---
+++ src/util/Input.java
@@ -0,0 +1,86 @@
+package util;
+import java.util.Scanner;
+
+
+public class Input {
+
+    private Scanner scanner;
+
+    public Input() {
+
+        this.scanner = new Scanner(System.in).useDelimiter("\n");
+
+    }
+
+    public String getString(){
+
+        System.out.println("Enter a String:");
+        return this.scanner.nextLine();
+
+    }
+
+    public boolean yesNo(){
+
+        System.out.println("[y/n]");
+        String input = this.scanner.nextLine();
+        return input.equals("y");
+
+    }
+
+    public int getInt(int min, int max){
+
+        System.out.println("Enter an int value min: " + min + " & max: " + max);
+
+        boolean correctInput = false;
+
+        int input;
+
+        do {
+            input = this.scanner.nextInt();
+            if (input >= min && input <= max){
+                correctInput = true;
+            } else {
+                System.out.println("Enter a value min: " + min + " & max: " + max);
+            }
+
+        } while (!correctInput);
+
+        return input;
+    }
+
+    public int getInt(){
+
+        System.out.println("Enter a number.");
+
+        return this.scanner.nextInt();
+    }
+
+    public double getDouble(double min, double max){
+
+        System.out.println("Enter a value min: " + min + " & max: " + max);
+
+        boolean correctInput = false;
+        double input;
+
+        do {
+            input = this.scanner.nextDouble();
+            if (input >= min && input <= max){
+                correctInput = true;
+            } else {
+                System.out.println("Enter a double value min: " + min + " & max: " + max);
+            }
+
+        } while (!correctInput);
+
+        return input;
+    }
+
+    public double getDouble(){
+
+        System.out.println("Try a double.");
+        return this.scanner.nextDouble();
+    }
+
+
+
+}
---
+++ src/util/InputTest.java
@@ -0,0 +1,23 @@
+package util;
+
+public class InputTest {
+
+    public static void main(String[] args) {
+
+        Input input = new Input();
+
+        System.out.println(input.getString());
+
+        System.out.println(input.yesNo());
+
+        System.out.println(input.getInt());
+
+        System.out.println(input.getInt(1,100));
+
+        System.out.println(input.getDouble());
+
+        System.out.println(input.getDouble(1,100));
+
+
+    }
+}
