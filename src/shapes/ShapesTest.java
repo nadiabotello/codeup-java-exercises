3413bfbe395d7a97b80920f442270a6fee5cdd27
Nadia Botello <botello.nadia.g@gmail.com>
2019-05-21 15:26:53 -0500

    updated sout

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -4,16 +4,20 @@ public class ShapesTest {
 
     public static void main(String[] args) {
 
-        Rectangle rectangle = new Rectangle(4, 5);
+        Rectangle box1 = new Rectangle(4, 5);
+        Rectangle box2 = new Square(5);
 
-        double box1 = rectangle.getPerimeter();
+//        double box1 = rectangle.getPerimeter();
 //        int box1 = rectangle.getArea();
-        System.out.println(box1);
+//        System.out.println(box1);
 
-        Rectangle square = new Square(5);
+//        Rectangle square = new Square(5);
 //        int box2 = square.getPerimeter();
-        double box2 = square.getArea();
-        System.out.println(box2);
+//        double box2 = square.getArea();
+//        System.out.println(box2);
+
+        System.out.println("Area is: " + box1.getArea() + " & Perimeter is: " + box1.getPerimeter());
+        System.out.println("Area is: " + box2.getArea() + " & Perimeter is: " + box2.getPerimeter());
 
     }
 
